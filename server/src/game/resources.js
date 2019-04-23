84a37e36ba75dcc2c7f0c12bca4a8b1a8efb83df
Sohrab Hosseini <sohrab.hosseini@gmail.com>
2019-04-23 21:15:46 +1000

    Support group of resources, e.g. <food>

--- server/src/game/resources.js
+++ server/src/game/resources.js
@@ -1,12 +1,27 @@
+const WILDCARDS = {
+  "<food>": ["fish", "meat", "bread"]
+};
+
 export function missingResources(required, resources) {
   return Object.entries(required)
-    .filter(([resource, amount]) => resources[resource] < amount)
+    .filter(([resource, amount]) =>
+      WILDCARDS[resource]
+        ? !WILDCARDS[resource].some(x => resources[x] >= amount)
+        : resources[resource] < amount
+    )
     .map(([resource]) => resource);
 }
 
 export function spendResources(cost, resources) {
   Object.entries(cost).forEach(([resource, amount]) => {
-    resources[resource] -= amount;
+    if (WILDCARDS[resource]) {
+      const actualResource = WILDCARDS[resource].find(
+        x => resources[x] >= amount
+      );
+      resources[actualResource] -= amount;
+    } else {
+      resources[resource] -= amount;
+    }
   });
 }
 
