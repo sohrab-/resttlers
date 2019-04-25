96dd0e77718638240153465baa5d373df7d7f184
Sohrab Hosseini <sohrab.hosseini@gmail.com>
2019-04-25 22:40:30 +1000

    Route UI and API docs requests

--- server/src/http/httpServer.js
+++ server/src/http/httpServer.js
@@ -1,9 +1,19 @@
+import fs from "fs";
+import path from "path";
 import fastify from "fastify";
+import fastifyStatic from "fastify-static";
 import fastifySensible from "fastify-sensible";
 
 import fastifyOpenapi from "@dpe/fastify-openapi";
 import { notFoundHandler, defaultErrorHandler } from "./error";
 import SettlementService from "../service/SettlementService";
+
+const BASE_DIR = path.join(__dirname, "..", "..", "..");
+const API_SPEC_YAML_PATH = path.join(BASE_DIR, "api.yaml");
+const apiSpecYaml = fs.readFileSync(API_SPEC_YAML_PATH);
+const apiSpecHtml = fs.readFileSync(
+  path.join(BASE_DIR, "server", "public", "apidocs.html")
+);
 
 /**
  * Configures and starts a Fastify server
@@ -12,7 +22,10 @@ import SettlementService from "../service/SettlementService";
  * @param {string} httpBasePath The HTTP base path applied to all resources
  * @param {string} logLevel The log level
  */
-export default function httpServer(game, { httpPort, httpBasePath, logLevel }) {
+export default function httpServer(
+  game,
+  { httpPort, httpBasePath = "", logLevel }
+) {
   const service = new SettlementService(game);
 
   const server = fastify({
@@ -24,12 +37,32 @@ export default function httpServer(game, { httpPort, httpBasePath, logLevel }) {
 
   server.register(fastifySensible, { errorHandler: false });
 
+  // serve API
   server.register(fastifyOpenapi, {
-    apiSpec: `${__dirname}/../../../api.yaml`,
+    apiSpec: API_SPEC_YAML_PATH,
     services: {
       "*": service
     },
-    basePath: httpBasePath
+    basePath: `${httpBasePath}/api`
+  });
+
+  // TODO admin endpoints
+
+  // serve UI
+  console.log(path.join(BASE_DIR, "client", "build"));
+  console.log(`${httpBasePath}/ui`);
+  server.register(fastifyStatic, {
+    root: path.join(BASE_DIR, "client", "build"),
+    prefix: `${httpBasePath}/ui`,
+    redirect: true
+  });
+
+  // serve API spec
+  server.get(`${httpBasePath}/apidocs`, (request, reply) => {
+    reply.type("text/html").send(apiSpecHtml);
+  });
+  server.get(`${httpBasePath}/apidocs.yaml`, (request, reply) => {
+    reply.type("text/yaml").send(apiSpecYaml);
   });
 
   server.setNotFoundHandler(notFoundHandler);
--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ import Game from "./game/Game";
 const LOG_LEVEL = process.env.LOG_LEVEL || "info";
 
 const HTTP_PORT = process.env.HTTP_PORT || "8085";
-const HTTP_BASE_PATH = process.env.HTTP_BASE_PATH || "/api";
+const HTTP_BASE_PATH = process.env.HTTP_BASE_PATH || "";
 
 const GAME_LOOP_DELAY = process.env.GAME_LOOP_DELAY || "1000";
 const BROADCAST_INTERVAL = process.env.BROADCAST_INTERVAL || "2000";
